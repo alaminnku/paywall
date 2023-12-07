66424ce3819c273672bcf75cd19b3fbcac54cf48
Alamin Shaikh <hello@octib.com>
2023-12-07 19:07:04 +0600

    Login page created

--- components/login/Form.tsx
+++ components/login/Form.tsx
@@ -1,5 +1,31 @@
+import Link from 'next/link';
 import styles from './Form.module.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '600'],
+});
 
 export default function Form() {
-  return <div>Form</div>;
+  return (
+    <section className={`${styles.container} ${inter.className}`}>
+      <form>
+        <input type='email' placeholder='Email' />
+        <input type='password' placeholder='Password' />
+        <input type='submit' />
+      </form>
+
+      <Link className={styles.forgot_password} href='/'>
+        Forgot password?
+      </Link>
+
+      <p className={styles.register}>
+        Don't have an account?{' '}
+        <Link className={styles.forgot_password} href='/'>
+          Register
+        </Link>
+      </p>
+    </section>
+  );
 }
