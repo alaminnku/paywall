eda511bdbc6db6eaf234f474f4e29fafe5ca9209
Alamin Shaikh <hello@octib.com>
2023-12-08 11:54:22 +0600

    Homepage features updated

--- components/home/Features.tsx
+++ components/home/Features.tsx
@@ -20,62 +20,129 @@ const inter = Inter({
 
 export default function Features() {
   return (
-    <section className={styles.container}>
-      <Image className={styles.block} src={block} alt='Block image' />
+    <>
+      <section className={styles.container_mobile}>
+        <Image className={styles.block} src={block} alt='Block image' />
 
-      <h2 className={pjs.className}>
-        The Fast-Track To Passive Income Is On The Other Side Of The Wall 💰
-      </h2>
+        <h2 className={pjs.className}>
+          The Fast-Track To Passive Income Is On The Other Side Of The Wall 💰
+        </h2>
 
-      <p className={styles.description}>
-        Don’t spend days trying to make internet money . Use the shortcut that
-        was built to turn visitors into paying customers! Paywall does it all
-        for you.
-      </p>
+        <p className={styles.description}>
+          Don't spend days trying to make internet money . Use the shortcut that
+          was built to turn visitors into paying customers! Paywall does it all
+          for you.
+        </p>
 
-      <Image
-        className={styles.phone}
-        src={phone}
-        alt='Phone showing subscription options'
-      />
+        <Image
+          className={styles.phone}
+          src={phone}
+          alt='Phone showing subscription options'
+        />
 
-      <FeatureCards />
+        <FeatureCards />
 
-      <div className={`${styles.feature_card} ${inter.className}`}>
-        <p className={styles.title}>Start Any One-Person Business</p>
+        <div className={`${styles.feature_card} ${inter.className}`}>
+          <p className={styles.title}>Start Any One-Person Business</p>
 
-        <div>
-          {featureLists.map(({ icon, text, price }, index) => (
-            <div className={styles.list} key={index}>
-              <div className={styles.detail}>
-                <span>{icon}</span>
-                <p>{text}</p>
+          <div>
+            {featureLists.map(({ icon, text, price }, index) => (
+              <div className={styles.list} key={index}>
+                <div className={styles.detail}>
+                  <span>{icon}</span>
+                  <p>{text}</p>
+                </div>
+                <p className={styles.price}>{price}</p>
               </div>
-              <p className={styles.price}>{price}</p>
+            ))}
+          </div>
+
+          <div className={styles.summary}>
+            <div className={styles.detail}>
+              <span>❌</span>
+              <p>What you'd spend otherwise</p>
             </div>
-          ))}
-        </div>
-
-        <div className={styles.summary}>
-          <div className={styles.detail}>
-            <span>❌</span>
-            <p>What you'd spend otherwise</p>
+            <p className={styles.price}>$240/mo</p>
           </div>
-          <p className={styles.price}>$240/mo</p>
-        </div>
 
-        <div className={styles.total}>
-          <div className={styles.detail}>
-            <span>🧱</span>
-            <p>Build your Paywall</p>
+          <div className={styles.total}>
+            <div className={styles.detail}>
+              <span>🧱</span>
+              <p>Build your Paywall</p>
+            </div>
+            <p>$19.99/mo</p>
           </div>
-          <p>$19.99/mo</p>
         </div>
-      </div>
 
-      <Link href='/' className={`${styles.cta} ${pjs.className}`}>
-        Start My Trial <FaArrowRight />
-      </Link>
-    </section>
+        <Link href='/' className={`${styles.cta} ${pjs.className}`}>
+          Start My Trial <FaArrowRight />
+        </Link>
+      </section>
+
+      <section className={styles.container_desktop}>
+        <div className={styles.content_and_image}>
+          <div className={styles.content}>
+            <h2 className={pjs.className}>
+              The Fast-Track To Passive Income Is On The Other Side Of The Wall
+              💰
+            </h2>
+
+            <Image className={styles.block} src={block} alt='Block image' />
+
+            <p className={styles.description}>
+              Don’t spend days trying to make internet money . Use the shortcut
+              that was built to turn visitors into paying customers! Paywall
+              does it all for you.
+            </p>
+
+            <FeatureCards />
+          </div>
+
+          <Image
+            className={styles.phone}
+            src={phone}
+            alt='Phone showing subscription options'
+          />
+        </div>
+
+        <div className={styles.cta_with_features}>
+          <div className={`${styles.feature_card} ${inter.className}`}>
+            <p className={styles.title}>Start Any One-Person Business</p>
+
+            <div>
+              {featureLists.map(({ icon, text, price }, index) => (
+                <div className={styles.list} key={index}>
+                  <div className={styles.detail}>
+                    <span>{icon}</span>
+                    <p>{text}</p>
+                  </div>
+                  <p className={styles.price}>{price}</p>
+                </div>
+              ))}
+            </div>
+
+            <div className={styles.summary}>
+              <div className={styles.detail}>
+                <span>❌</span>
+                <p>What you'd spend otherwise</p>
+              </div>
+              <p className={styles.price}>$240/mo</p>
+            </div>
+
+            <div className={styles.total}>
+              <div className={styles.detail}>
+                <span>🧱</span>
+                <p>Build your Paywall</p>
+              </div>
+              <p>$19.99/mo</p>
+            </div>
+          </div>
+
+          <Link href='/' className={`${styles.cta} ${pjs.className}`}>
+            Start My Trial <FaArrowRight />
+          </Link>
+        </div>
+      </section>
+    </>
   );
 }
